13df91c78e36fa193560480d65ceda87e995d02e
donwung <113479299+donwung@users.noreply.github.com>
2024-04-02 15:58:40 -0700

    added preview images

--- NeopetsPAHGeneratorPlusR/src/components/EditableListing.jsx
+++ NeopetsPAHGeneratorPlusR/src/components/EditableListing.jsx
@@ -8,43 +8,51 @@ const EditableListing = (props) => {
     const [isCustom, setIsCustom] = useState(listing.isCustom)
     const [hasAgeTrophy, setHasAgeTrophy] = useState(listing.hasAgeTrophy)
     const [canInit, setCanInit] = useState(listing.canInit)
+    const [imageSource, setImageSource] = useState(`http://pets.neopets.com/cpn/${petName}/1/4.png`)
 
     useEffect(() => {
-        // console.log("changed")
-        editListingAtIdx({isCustom: isCustom, hasAgeTrophy: hasAgeTrophy, canInit: canInit}, index)
+        editListingAtIdx({
+            isCustom: isCustom,
+            hasAgeTrophy: hasAgeTrophy,
+            canInit: canInit
+        }, index)
     }, [isCustom, hasAgeTrophy, canInit]);
 
+    useEffect(()=> {
+        console.log("rerender")
+        setImageSource(`http://pets.neopets.com/cpn/${petName}/1/4.png`)
+    }, [petName])
+
+    const renderFallbackImg = () => {
+        setImageSource(`https://images.neopets.com/caption/caption_1180.gif`)
+    }
+
+
     return <>
-        <div key={index}>
-            <h3>petname: {petName}</h3>
-            <div>
-                <div style={{ display: "flex", flexDirection: "column", textAlign: "left"}}>
-                    <div style={{display: "flex", justifyContent: "space-between"}}>
-                        Is Custom
-                        <input
-                            type='checkbox'
-                            checked={isCustom}
-                            onChange={() => setIsCustom(!isCustom)}></input>
-                    </div>
-                    <div style={{display: "flex", justifyContent: "space-between"}}>
-                        Has Age Trophy
-                        <input
-                            type='checkbox'
-                            checked={hasAgeTrophy}
-                            onChange={() => setHasAgeTrophy(!hasAgeTrophy)}></input>
-                    </div>
-                    <div style={{display: "flex", justifyContent: "space-between"}}>
-                        Cannot Initiate
-                        <input
-                            type='checkbox'
-                            checked={!canInit}
-                            onChange={() => setCanInit(!canInit)}></input>
-                    </div>
-                </div>
-                <p>
-                    detail: {petDetail}
-                </p>
+        <div className="listing-setting-container" key={index}>
+            <div className="d-flex justify-between">
+                Is Custom
+                <input
+                    type='checkbox'
+                    checked={isCustom}
+                    onChange={() => setIsCustom(!isCustom)}></input>
             </div>
+            <div className="d-flex justify-between">
+                Has Age Trophy
+                <input
+                    type='checkbox'
+                    checked={hasAgeTrophy}
+                    onChange={() => setHasAgeTrophy(!hasAgeTrophy)}></input>
+            </div>
+            <div className="d-flex justify-between">
+                Cannot Initiate
+                <input
+                    type='checkbox'
+                    checked={!canInit}
+                    onChange={() => setCanInit(!canInit)}></input>
+            </div>
+            <h3>{petName} {petDetail && `- ${petDetail}`}</h3>
+            <img onError={()=>renderFallbackImg()} src={imageSource}></img>
         </div>
     </>
 }
