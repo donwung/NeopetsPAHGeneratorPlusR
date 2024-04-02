524973f5ead4d8b7756e65370f1f824d0eea02da
donwung <113479299+donwung@users.noreply.github.com>
2024-04-02 14:25:51 -0700

    separated output code from jsx

--- NeopetsPAHGeneratorPlusR/src/components/SideBySideCode.jsx
+++ NeopetsPAHGeneratorPlusR/src/components/SideBySideCode.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { generateCopyPastableHTML_stacked, generateCopyPastableHTML_SBS } from "./PrintScripts"
 
 const SideBySideCode = (props) => {
     const { listings, username } = props
@@ -6,111 +7,12 @@ const SideBySideCode = (props) => {
     const [copyPastableHTML_stacked, setCopyPastableHTML_stacked] = useState("")
     const [copyPastableHTML_SBS, setCopyPastableHTML_SBS] = useState("")
 
-    const generateCopyPastableHTML_stacked = () => {
-        let pastableString = ""
-
-        if (listings.length > 0) {
-            for (let i = 0; i < listings.length; i++) {
-                const petName = listings[i].petName
-                const petDetail = listings[i].petDetail
-                pastableString += `<a href="/petlookup.phtml?pet=${petName}">\n<img src="//pets.neopets.com/cpn/${petName}/1/4.png">\n</a><br>${petName}`
-
-                if (petDetail != "") {
-                    pastableString += ` - ${petDetail}\n`
-                } else {
-                    pastableString += `\n`
-                }
-
-                if (i != listings.length - 1) {
-                    pastableString += `<br>\n`
-                }
-
-                if (listings[i].hasAgeTrophy) {
-                    pastableString += `<img src="https://images.neopets.com/games/trophies/trophy_oldpet_1.gif" style="width: 25px; height: 25px;"></img>\n`
-                }
-                if (listings[i].canInit) {
-                    pastableString += `<img src="https://images.neopets.com/halloween/spooky_suprise/dd_close_box.png" style="width: 25px; height: 25px;"></img>\n`
-                }
-
-            }
-            pastableString += `<h2 style="border-bottom: 1px solid #818589; margin-bottom: 25px; padding-bottom:5px;">\n<a href="https://www.neopets.com/neomessages.phtml?type=send&recipient=${username}">\n@${username}\n</a>\n</h2>`
-
-            setCopyPastableHTML_stacked(pastableString)
-        }
-    }
-
-    const generateCopyPastableHTML_SBS = () => {
-        let pastableString = ""
-
-        if (listings.length > 0) {
-            const listing1 = listings[0]
-            const listing2 = listings[1]
-
-            // LISTING LEFT
-            pastableString += `<div style="width: 100%; overflow: hidden;">`
-
-            pastableString += `\n<div style="width: 48%; float: left;">\n<div style="padding-left: 15%;">\n`
-
-            if (listing1.isCustom) {
-                pastableString += `<img src="https://images.neopets.com/caption/caption_1180.gif" style="width: 300px; height: 300px;">\n`
-            } else {
-                pastableString += `<a href="/petlookup.phtml?pet=${listing1.petName}">\n<img src="http://pets.neopets.com/cpn/${listing1.petName}/1/4.png">\n</a>`
-            }
-
-            pastableString += `<br>${listing1.petName}`
-
-            if (listing1.petDetail != "") {
-                pastableString += ` - ${listing1.petDetail}\n`
-            } else {
-                pastableString += `\n`
-            }
-
-            if (listing1.hasAgeTrophy) {
-                pastableString += `<img src="https://images.neopets.com/games/trophies/trophy_oldpet_1.gif" style="width: 25px; height: 25px;"></img>\n`
-            }
-            if (!listing1.canInit) {
-                pastableString += `<img src="https://images.neopets.com/halloween/spooky_suprise/dd_close_box.png" style="width: 25px; height: 25px;"></img>\n`
-            }
-
-            pastableString += `</div></div>`
-
-
-            // LISTING RIGHT
-            pastableString += `\n<div style="width: 48%; float: right;">\n<div style="padding-right: 15%;">\n`
-
-            if (listing2.isCustom) {
-                pastableString += `<img src="https://images.neopets.com/caption/caption_1180.gif" style="width: 300px; height: 300px;">\n`
-            } else {
-                pastableString += `<a href="/petlookup.phtml?pet=${listing2.petName}">\n<img src="http://pets.neopets.com/cpn/${listing2.petName}/1/4.png">\n</a>`
-            }
-            pastableString += `<br>${listing2.petName}`
-
-            if (listing2.petDetail != "") {
-                pastableString += ` - ${listing2.petDetail}\n`
-            } else {
-                pastableString += `\n`
-            }
-
-            if (listing2.hasAgeTrophy) {
-                pastableString += `<img src="https://images.neopets.com/games/trophies/trophy_oldpet_1.gif" style="width: 25px; height: 25px;"></img>\n`
-            }
-            if (!listing2.canInit) {
-                pastableString += `<img src="https://images.neopets.com/halloween/spooky_suprise/dd_close_box.png" style="width: 25px; height: 25px;"></img>\n`
-            }
-
-            pastableString += `</div></div></div>\n`
-
-            pastableString += `<br>`
-
-            pastableString += `<h2 style="border-bottom: 1px solid #818589; margin-bottom: 25px; padding-bottom: 5px;">\n<a href="http://www.neopets.com/neomessages.phtml?type=send&recipient=${username}">\n@${username}\n</a>\n</h2>`
-        }
-
-        setCopyPastableHTML_SBS(pastableString)
-    }
-
     useEffect(() => {
-        generateCopyPastableHTML_stacked()
-        generateCopyPastableHTML_SBS()
+        const pastableString_stacked = generateCopyPastableHTML_stacked(listings, username)
+        const pastableString_SBS = generateCopyPastableHTML_SBS(listings, username)
+
+        setCopyPastableHTML_stacked(pastableString_stacked)
+        setCopyPastableHTML_SBS(pastableString_SBS)
     }, [listings])
 
     return <>
--- NeopetsPAHGeneratorPlusR/src/components/StackedOrSingleCode.jsx
+++ NeopetsPAHGeneratorPlusR/src/components/StackedOrSingleCode.jsx
@@ -1,54 +1,15 @@
 import { useEffect, useState } from "react"
+import { generateCopyPastableHTML_stacked } from "./PrintScripts"
 
 const StackedOrSingleCode = (props) => {
     const { listings, username } = props
     const [copyPastableHTML, setCopyPastableHTML] = useState("")
 
     useEffect(() => {
-        let pastableString = ""
-
-        if (listings.length > 0) {
-            for (let i = 0; i < listings.length; i++) {
-                const petName = listings[i].petName
-                const petDetail = listings[i].petDetail
-                const petSettings = {
-                    isCustom: listings[i].isCustom,
-                    hasAgeTrophy: listings[i].hasAgeTrophy,
-                    canInit: listings[i].canInit,
-                }
-
-                if (petSettings.isCustom) {
-                    pastableString += `<img src="https://images.neopets.com/caption/caption_1180.gif" style="width: 300px; height: 300px;">\n`
-                } else {
-                    pastableString += `<a href="/petlookup.phtml?pet=${petName}">\n<img src="//pets.neopets.com/cpn/${petName}/1/4.png">\n</a><br>${petName} `
-                    if (petDetail != "") {
-                        pastableString += ` - ${petDetail}\n`
-                    } else {
-                        pastableString += `\n`
-                    }
-                }
-
-                if (listings[i].hasAgeTrophy) {
-                    pastableString += `<img src="https://images.neopets.com/games/trophies/trophy_oldpet_1.gif" style="width: 25px; height: 25px;"></img>\n`
-                }
-                if (!listings[i].canInit) {
-                    pastableString += `<img src="https://images.neopets.com/halloween/spooky_suprise/dd_close_box.png" style="width: 25px; height: 25px;"></img>\n`
-                }
-
-                if (i != listings.length - 1) {
-                    pastableString += `<br>\n\n`
-                }
-
-
-            }
-            pastableString += `<h2 style="border-bottom: 1px solid #818589; margin-bottom: 25px; padding-bottom:5px;">\n<a href="https://www.neopets.com/neomessages.phtml?type=send&recipient=${username}">\n@${username}\n</a>\n</h2>`
-        }
-
+        const pastableString = generateCopyPastableHTML_stacked(listings, username)
         setCopyPastableHTML(pastableString)
-
         console.log(pastableString)
     }, [listings])
-
 
     return <>
         <div style={{ display: "flex", justifyContent: "space-around", gap: "20px", width: "60vw", margin: "auto" }}>
@@ -61,12 +22,15 @@ const StackedOrSingleCode = (props) => {
                     placeholder='disabled by having more or less than two listings'
                     disabled="true"></textarea>
             </div>
-            <div style={{ width: "50%" }}>
-                <h2>
-                    stacked listings
-                </h2>
+            <div style={{ width: "50%", border: "2px solid white" }}>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    <h2>
+                        Listings in a column
+                    </h2>
+                    <button>Copy to Clipboard</button>
+                </div>
                 <textarea
-                    style={{ width: "100%", height: "300px" }}
+                    style={{ width: "100%", height: "300px", resize: "none" }}
                     placeholder='stacked code here'
                     value={copyPastableHTML}
                     onChange={e => setCopyPastableHTML(e.target.value)}></textarea>
